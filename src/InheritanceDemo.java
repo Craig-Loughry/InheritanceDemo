5b83fa233df353a87517be08e691ed2307c0f6a5
Craig-Loughry <c_loughry@outlook.com>
2018-04-30 21:07:00 -0400

    First commit

---
+++ src/InheritanceDemo.java
@@ -0,0 +1,22 @@
+
+public class InheritanceDemo {
+
+					public static void main(String[] args) {
+						System.out.println("Hello");
+						
+						Product jakesBook = new Product("java", "Murach's Java", 54.50);
+						Product chrissBook = new Product("java", "Murach's Java", 54.50);
+						
+						System.out.println(jakesBook.hashCode());
+						System.out.println(chrissBook.getClass());
+						System.out.println(jakesBook);
+						
+						System.out.println("Bye");
+					}
+
+				}
+
+
+	}
+
+}
